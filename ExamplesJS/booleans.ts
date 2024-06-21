abd7fa1e5435e8199847b2bb580be9d6d2cafa7a
Talisson Santos <tsantosadm@gmail.com>
2024-06-21 15:49:33 -0300

    clean code with boolean

---
+++ ExamplesJS/booleans.ts
@@ -0,0 +1,37 @@
+// Booleanos (True/False)
+
+//Variable boolean semantic error
+const userMajority = true
+
+// variable boolean semantic correct
+const isUserOnMajority = true
+
+// Flags que podem ser usadas [is, does. has]
+
+// error
+const disabled = true;
+//correct
+const isDisabled = true
+
+const userHasGroup = true
+
+// -----------------------------
+
+// Sempre buscar semântica usando IF/SE (Se está...Se é)
+if (isUserOnMajority){
+
+}
+
+// Booleans sempre como perguntas / Semânticos
+loading
+isLoading
+
+//Nomenclaturas
+loading
+isLoading
+
+isUserProfileLoading
+
+//loading, disabled, data, fetching, list
+// booleanos como perguntas
+// if loading, if is user profile loading
