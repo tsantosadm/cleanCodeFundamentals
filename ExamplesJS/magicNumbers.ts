07942fb5e0a8f97bc510df3336b2c0ab98a916d1
Talisson Santos <tsantosadm@gmail.com>
2024-08-02 12:41:18 -0300

    magicNumbers

---
+++ ExamplesJS/magicNumbers.ts
@@ -0,0 +1,16 @@
+// Simples
+
+
+//setTimeout, setInterval
+const thirty_days = 1000 * 60 * 60 * 24 * 30
+
+setTimeout(() => {
+
+}, thirty_days) // trinta dias
+
+
+//Avançado
+
+function calculateDiscount(priceInCents, discountAmountInPercentage){
+    // retorna desconto
+}
